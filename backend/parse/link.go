36e888bdd3d58fd56453743cc4c74dd3f1821feb
BaiMeow <baishu2003@outlook.com>
2023-05-10 21:56:07 +0800

    feat: use arrow line when cost is diffed

--- backend/parse/link.go
+++ backend/parse/link.go
@@ -1,7 +1,5 @@
 package parse
 
-import "strings"
-
 type Link struct {
 	Src  string `json:"src,omitempty"`
 	Dst  string `json:"dst,omitempty"`
@@ -9,8 +7,5 @@ type Link struct {
 }
 
 func newLink(src, dst string, cost int) Link {
-	if strings.Compare(src, dst) == 1 {
-		src, dst = dst, src
-	}
 	return Link{src, dst, cost}
 }
