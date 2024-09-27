33f6c4228c15d11cb5a59c57b8455fe2d7b7232b
sd0ric4 <1286518974@qq.com>
2024-09-28 01:50:10 +0800

    refactor: 更新前端代理配置
    更新前端vite.config.ts文件中的代理配置，将目标地址修改为本地开发环境的地址。

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -37,11 +37,11 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://monitor.dn11.baimeow.cn",
+        target: "http://127.0.0.1:8787",
         changeOrigin: true,
       },
       "/monitor-metadata.json": {
-        target: "https://monitor.dn11.baimeow.cn",
+        target: "http://127.0.0.1:8787",
         changeOrigin: true,
       },
     },
