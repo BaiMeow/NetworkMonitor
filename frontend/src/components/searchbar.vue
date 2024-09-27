262d994c05bea28583884774fee69df44dfd90c1
sd0ric4 <1286518974@qq.com>
2024-09-27 22:13:54 +0800

    refactor: 优化代码结构和格式
    - 优化代码结构和格式，包括添加缺失的分号、修复缩进、删除多余的空格等
    - 修复README.md中的路径描述，因为并不存在`/backend/config/`这个路径，只存在`/backend/conf`
    - 更新样式文件，添加暗黑模式支持
    - 更新vite配置，将代理目标改为https://monitor.dn11.baimeow.cn

--- frontend/src/components/searchbar.vue
+++ frontend/src/components/searchbar.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-import { Netmask } from 'netmask';
-import { ref, Ref, watch } from 'vue';
+import { Netmask } from "netmask";
+import { ref, Ref, watch } from "vue";
 export interface selectItem {
   asn?: string;
   name?: string;
@@ -11,69 +11,93 @@ export interface selectItem {
 }
 
 const props = defineProps<{
-  data: Array<selectItem>
+  data: Array<selectItem>;
 }>();
 
 const options: Ref<Array<selectItem>> = ref([]);
 
-const input: Ref<string> = ref('');
+const input: Ref<string> = ref("");
 
 watch(
   () => props.data,
   () => search(input.value)
-)
+);
 
 const onchange = (value: string) => {
-  props.data.find((v) => {
-    return v.value === value
-  })?.onselected()
-}
+  props.data
+    .find((v) => {
+      return v.value === value;
+    })
+    ?.onselected();
+};
 
 function search(val: string) {
   if (!val) {
-    options.value = props.data
-    return
+    options.value = props.data;
+    return;
   }
   options.value = props.data.filter((v) => {
-    return v.label.toLowerCase().includes(val.toLowerCase()) || v.value.toLowerCase().includes(val.toLowerCase()) || v.network?.some((n) => n.includes(val)) || (() => {
-      try {
-        const nums = val.split('.')
-        if (nums.length !== 4) {
-          for (let i = nums.length; i < 4; i++) {
-            nums.push('0')
+    return (
+      v.label.toLowerCase().includes(val.toLowerCase()) ||
+      v.value.toLowerCase().includes(val.toLowerCase()) ||
+      v.network?.some((n) => n.includes(val)) ||
+      (() => {
+        try {
+          const nums = val.split(".");
+          if (nums.length !== 4) {
+            for (let i = nums.length; i < 4; i++) {
+              nums.push("0");
+            }
           }
+          const sr = new Netmask(nums.join("."));
+          return v.network?.some((n) => new Netmask(n).contains(sr.base));
+        } catch {
+          return false;
         }
-        const sr = new Netmask(nums.join('.'))
-        return v.network?.some((n) => new Netmask(n).contains(sr.base))
-      } catch {
-        return false
-      }
-    })()
-  })
+      })()
+    );
+  });
 }
-
 </script>
 
 <template>
-  <div style="display:flex;align-items: center">
-    <el-icon color="--el-color-primary" style="margin-right:.5rem">
+  <div style="display: flex; align-items: center">
+    <el-icon color="--el-color-primary" style="margin-right: 0.5rem">
       <i-ep-search />
     </el-icon>
-    <el-select v-model="input" @blur="input=''" filterable placeholder="Search" @change="onchange"
-      :filter-method="search">
-      <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
-        <span style="float: left;width:40%;margin-right:1rem">{{ item.label }}</span>
-        <span v-if="item.label !== item.value" style="
-          float: right;
-          color: var(--el-text-color-secondary);
-          font-size: 13px;
-        ">
+    <el-select
+      v-model="input"
+      @blur="input = ''"
+      filterable
+      placeholder="Search"
+      @change="onchange"
+      :filter-method="search"
+    >
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+        <span style="float: left; width: 40%; margin-right: 1rem">{{
+          item.label
+        }}</span>
+        <span
+          v-if="item.label !== item.value"
+          style="
+            float: right;
+            color: var(--el-text-color-secondary);
+            font-size: 13px;
+          "
+        >
           {{ item.value }}
         </span>
       </el-option>
     </el-select>
   </div>
-
 </template>
 
-<style scoped></style>
+<style scoped>
+@media (prefers-color-scheme: dark) {
+}
+</style>
