27f7ab110c9bd914e146c414830ac021e2e76a34
sd0ric4 <1286518974@qq.com>
2024-09-27 22:13:54 +0800

    refactor: 优化代码结构和格式
    - 优化代码结构和格式，包括添加缺失的分号、修复缩进、删除多余的空格等
    - 修复README.md中的路径描述，因为并不存在`/backend/config/`这个路径，只存在`/backend/conf`
    - 更新样式文件，添加暗黑模式支持
    - 更新vite配置，将代理目标改为https://monitor.dn11.baimeow.cn

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,37 +1,39 @@
 <script setup lang="ts">
-import BGP from "./components/BGP.vue"
-import OSPF from "./components/OSPF.vue"
+import BGP from "./components/BGP.vue";
+import OSPF from "./components/OSPF.vue";
 import { getList } from "./api/list";
 import { ASData, loadASData } from "./api/meta";
-import { provide, ref, reactive } from "vue"
-import { ASDataKey } from "./inject/key"
+import { provide, ref, reactive } from "vue";
+import { ASDataKey } from "./inject/key";
 
 const asn = ref(0);
-const graph_type = ref('');
-const menu_rotate = ref('rotate-closed-margin');
+const graph_type = ref("");
+const menu_rotate = ref("rotate-closed-margin");
 const loading = ref(true);
 const dataReady = ref(false);
-const loaded = () => { loading.value = false }
-const menu_scale = ref('');
+const loaded = () => {
+  loading.value = false;
+};
+const menu_scale = ref("");
 const click_fold = () => {
-  if (menu_rotate.value == 'rotate-open') {
-    menu_rotate.value = 'rotate-close rotate-closed-margin';
-    menu_scale.value = 'menu-fold'
+  if (menu_rotate.value == "rotate-open") {
+    menu_rotate.value = "rotate-close rotate-closed-margin";
+    menu_scale.value = "menu-fold";
   } else {
-    menu_rotate.value = 'rotate-open';
-    menu_scale.value = 'menu-expend';
+    menu_rotate.value = "rotate-open";
+    menu_scale.value = "menu-expend";
   }
-}
+};
 
 const asdata = ref({} as ASData | null);
 provide(ASDataKey, asdata);
 
 class bgp {
   enable() {
-    graph_type.value = "bgp"
+    graph_type.value = "bgp";
   }
   display() {
-    return "BGP FULL GRAPH"
+    return "BGP FULL GRAPH";
   }
 }
 
@@ -42,57 +44,57 @@ class ospf {
     this.asn = asn;
   }
   async init() {
-    this.name = asdata.value?.metadata?.[this.asn + ""]?.display || ""
+    this.name = asdata.value?.metadata?.[this.asn + ""]?.display || "";
   }
   enable() {
-    graph_type.value = "ospf"
-    asn.value = this.asn
+    graph_type.value = "ospf";
+    asn.value = this.asn;
   }
   display() {
-    return this.name ? `${this.name} Network` : `AS ${this.asn}`
+    return this.name ? `${this.name} Network` : `AS ${this.asn}`;
   }
 }
 
 interface graph {
-  enable(): void
-  display(): string
+  enable(): void;
+  display(): string;
 }
 
-const graph_list = reactive([] as Array<graph>)
+const graph_list = reactive([] as Array<graph>);
 
 const handle_select = (idx: string) => {
   loading.value = true;
-  graph_list[+idx].enable()
-}
+  graph_list[+idx].enable();
+};
 
 (async () => {
   const data = await loadASData();
   if (data) {
-    asdata.value = data
+    asdata.value = data;
   }
   const list = await getList();
-  
+
   list.forEach((graph) => {
     switch (graph.type) {
       case "bgp":
-        graph_list.push(new bgp())
-        break
+        graph_list.push(new bgp());
+        break;
       case "ospf":
         const gr = new ospf(graph.asn);
-        gr.init()
-        graph_list.push(gr)
-        break
+        gr.init();
+        graph_list.push(gr);
+        break;
     }
-  })
+  });
 
   graph_list.sort((a, b) => {
     if (a instanceof ospf && b instanceof bgp) return 1;
     if (b instanceof bgp && a instanceof ospf) return -1;
     return a.display().localeCompare(b.display());
-  })
+  });
 
   if (graph_list.length !== 0) {
-    graph_list[0]?.enable()
+    graph_list[0]?.enable();
   } else {
     alert("no data");
   }
@@ -102,22 +104,40 @@ const handle_select = (idx: string) => {
 <template>
   <div class="aside">
     <div class="menu">
-      <el-button type="primary" circle class="menu-button transition-06s" :class="menu_rotate" @click="click_fold">
+      <el-button
+        type="primary"
+        circle
+        class="menu-button transition-06s"
+        :class="menu_rotate"
+        @click="click_fold"
+      >
         <i-ep-arrow-right-bold />
       </el-button>
-      <el-menu :collapse-transition=false class="menu-list transition-06s" :class="menu_scale" default-active="0"
-        @select="handle_select">
-        <el-menu-item class="menu-item" v-for="(graph, index) in graph_list" :index="index.toString()">
+      <el-menu
+        :collapse-transition="false"
+        class="menu-list transition-06s"
+        :class="menu_scale"
+        default-active="0"
+        @select="handle_select"
+      >
+        <el-menu-item
+          class="menu-item"
+          v-for="(graph, index) in graph_list"
+          :index="index.toString()"
+        >
           <span>{{ graph.display() }}</span>
         </el-menu-item>
       </el-menu>
     </div>
   </div>
   <template v-if="dataReady">
-    <OSPF v-if="graph_type === 'ospf' && asn != null && dataReady" :asn="asn" :loaded="loaded" />
+    <OSPF
+      v-if="graph_type === 'ospf' && asn != null && dataReady"
+      :asn="asn"
+      :loaded="loaded"
+    />
     <BGP v-else-if="graph_type === 'bgp' && dataReady" :loaded="loaded" />
   </template>
-
 </template>
 
 <style scoped>
@@ -166,7 +186,7 @@ const handle_select = (idx: string) => {
 }
 
 .rotate-closed-margin {
-  margin: 0px !important
+  margin: 0px !important;
 }
 
 .menu-item {
@@ -205,5 +225,24 @@ const handle_select = (idx: string) => {
 .menu-expend {
   max-height: 70vh;
   max-width: 12em;
+}
+@media (prefers-color-scheme: dark) {
+  .menu {
+    background-color: rgba(0, 0, 0, 0.5);
+    box-shadow: 1.5px 1.5px 1.5px 1.5px rgba(0, 0, 0, 0.3);
+    color: white;
+  }
+  .menu-item {
+    color: wheat;
+  }
+  .menu-item:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+  .el-button--primary {
+    border-color: rgba(255, 255, 255, 0.5);
+  }
+  .menu-button {
+    background-color: rgba(0, 0, 0, 0.5);
+  }
 }
 </style>
