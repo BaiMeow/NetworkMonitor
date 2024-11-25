f9fae8c8362daab441cb5ff6355722449fb40c56
柏喵Sakura <38121125+BaiMeow@users.noreply.github.com>
2024-11-25 14:14:50 +0000

    fix: closeness algo

--- backend/graph/analysis/closeness.go
+++ backend/graph/analysis/closeness.go
@@ -8,15 +8,24 @@ type ClosenessResult struct {
 func (g *Graph) Closeness() []ClosenessResult {
 	var result []ClosenessResult
 	for _, node := range g.Nodes {
-		var sum float64
 		paths := g.SingleSourceShortestPaths(node)
+		distance := make([]int, len(g.Nodes))
 		for _, path := range paths {
-			if path.Length == 0 {
-				continue
-			}
-			sum += float64(path.Length)
+			distance[path.Dst.Id] = path.Length
 		}
-		result = append(result, ClosenessResult{Node: node, Closeness: float64(len(g.Nodes)-1) / sum})
+		var sum int
+		for i, v := range distance {
+			if v == 0 && i != node.Id {
+				sum = -1
+				break
+			}
+			sum += v
+		}
+		if sum == -1 {
+			// graph not full connected, not supported
+			result = append(result, ClosenessResult{Node: node, Closeness: -1})
+		}
+		result = append(result, ClosenessResult{Node: node, Closeness: float64(len(g.Nodes)-1) / float64(sum)})
 	}
 	return result
 }
