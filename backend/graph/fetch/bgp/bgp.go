529c5a3390ec76f5e31bcd7d690d6d91574af786
BaiMeow <baishu2003@outlook.com>
2025-02-20 22:34:56 +0800

    fix: bgp probe fail with port reused

--- backend/graph/fetch/bgp/bgp.go
+++ backend/graph/fetch/bgp/bgp.go
@@ -4,26 +4,22 @@ import (
 	"context"
 	"fmt"
 	"github.com/BaiMeow/NetworkMonitor/graph/fetch"
+	"github.com/BaiMeow/NetworkMonitor/utils"
 	apipb "github.com/osrg/gobgp/v3/api"
 	gobgplog "github.com/osrg/gobgp/v3/pkg/log"
 	"github.com/osrg/gobgp/v3/pkg/server"
 	"github.com/pkg/errors"
 	"log"
-	"math"
 	"net/netip"
 	"time"
 )
 
 func init() {
 	fetch.Register("bgp", func(m map[string]any) (fetch.Fetcher, error) {
-		asnRaw, ok := m["asn"].(int)
-		if !ok {
-			return nil, fmt.Errorf("asn is not int")
+		asn, err := utils.MustASN(m["asn"])
+		if err != nil {
+			return nil, fmt.Errorf("asn is not valid asn: %v", err)
 		}
-		if asnRaw > math.MaxUint32 || asnRaw < 0 {
-			return nil, fmt.Errorf("invalid asn number: %d", asnRaw)
-		}
-		asn := uint32(asnRaw)
 
 		routerIdRaw, ok := m["router-id"].(string)
 		if !ok {
@@ -58,107 +54,141 @@ func init() {
 		logger := gobgplog.NewDefaultLogger()
 		logger.SetLevel(gobgplog.ErrorLevel)
 		bgpServer := server.NewBgpServer(server.LoggerOption(logger))
+		ctx, cancel := context.WithCancel(context.Background())
+		bgp := &BGP{
+			s:      bgpServer,
+			cancel: cancel,
+		}
 		go bgpServer.Serve()
-		if err := bgpServer.StartBgp(context.Background(), &apipb.StartBgpRequest{
-			Global: &apipb.Global{
-				Asn:              asn,
-				RouterId:         routerIdRaw,
-				ListenPort:       int32(port),
-				UseMultiplePaths: true,
-			},
-		}); err != nil {
-			return nil, err
-		}
-
-		if mode == "listen" {
-			if err := bgpServer.AddPeerGroup(context.Background(), &apipb.AddPeerGroupRequest{
-				PeerGroup: &apipb.PeerGroup{
-					Conf: &apipb.PeerGroupConf{
-						PeerGroupName: "route-collector",
-						LocalAsn:      asn,
+		go func() {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+				if err := bgpServer.StartBgp(ctx, &apipb.StartBgpRequest{
+					Global: &apipb.Global{
+						Asn:              asn,
+						RouterId:         routerIdRaw,
+						ListenPort:       int32(port),
+						UseMultiplePaths: true,
 					},
-					EbgpMultihop: &apipb.EbgpMultihop{
-						Enabled: true,
-					},
-					AfiSafis: []*apipb.AfiSafi{{
-						Config: &apipb.AfiSafiConfig{
-							Family: &apipb.Family{
-								Afi:  apipb.Family_AFI_IP,
-								Safi: apipb.Family_SAFI_UNICAST,
-							},
-						},
-						AddPaths: &apipb.AddPaths{
-							Config: &apipb.AddPathsConfig{
-								Receive: true,
-							},
-						},
-					}},
-				},
-			}); err != nil {
-				return nil, err
-			}
-			if err := bgpServer.AddDynamicNeighbor(context.Background(), &apipb.AddDynamicNeighborRequest{
-				DynamicNeighbor: &apipb.DynamicNeighbor{
-					Prefix:    "0.0.0.0/0",
-					PeerGroup: "route-collector",
-				},
-			}); err != nil {
-				return nil, err
-			}
-		} else {
-			peerASN, ok := m["peer-asn"].(int)
-			if !ok {
-				return nil, fmt.Errorf("peer-asn is not int")
-			}
-			if peerASN > math.MaxUint32 || peerASN < 0 {
-				return nil, fmt.Errorf("invalid peer-asn number: %d", asnRaw)
+				}); err != nil {
+					log.Printf("start bgp fail: %v", err)
+					time.Sleep(time.Second * 3)
+					continue
+				}
+				break
 			}
 
-			neighborAddr, ok := m["neighbor-addr"].(string)
-			if !ok {
-				return nil, fmt.Errorf("neighbor-addr is not string")
-			}
-			if _, err := netip.ParseAddr(neighborAddr); err != nil {
-				return nil, fmt.Errorf("invalid neighbor-addr: %s", neighborAddr)
-			}
-
-			if err := bgpServer.AddPeer(context.Background(), &apipb.AddPeerRequest{
-				Peer: &apipb.Peer{
-					Conf: &apipb.PeerConf{
-						PeerAsn:         uint32(peerASN),
-						NeighborAddress: neighborAddr,
-					},
-					EbgpMultihop: &apipb.EbgpMultihop{
-						Enabled: true,
-					},
-					AfiSafis: []*apipb.AfiSafi{{
-						Config: &apipb.AfiSafiConfig{
-							Family: &apipb.Family{
-								Afi:  apipb.Family_AFI_IP,
-								Safi: apipb.Family_SAFI_UNICAST,
-							},
+			if mode == "listen" {
+				if err := bgpServer.AddPeerGroup(context.Background(), &apipb.AddPeerGroupRequest{
+					PeerGroup: &apipb.PeerGroup{
+						Conf: &apipb.PeerGroupConf{
+							PeerGroupName: "route-collector",
+							LocalAsn:      asn,
 						},
-						AddPaths: &apipb.AddPaths{
-							Config: &apipb.AddPathsConfig{
-								Receive: true,
-							},
+						EbgpMultihop: &apipb.EbgpMultihop{
+							Enabled: true,
 						},
-					}},
-				},
-			}); err != nil {
-				return nil, err
+						AfiSafis: []*apipb.AfiSafi{{
+							Config: &apipb.AfiSafiConfig{
+								Family: &apipb.Family{
+									Afi:  apipb.Family_AFI_IP,
+									Safi: apipb.Family_SAFI_UNICAST,
+								},
+							},
+							AddPaths: &apipb.AddPaths{
+								Config: &apipb.AddPathsConfig{
+									Receive: true,
+								},
+							},
+						}},
+					},
+				}); err != nil {
+					log.Printf("add peer group fail: %v", err)
+					if err := bgp.CleanUp(); err != nil {
+						log.Printf("cleaup: %v", err)
+					}
+					return
+				}
+				if err := bgpServer.AddDynamicNeighbor(context.Background(), &apipb.AddDynamicNeighborRequest{
+					DynamicNeighbor: &apipb.DynamicNeighbor{
+						Prefix:    "0.0.0.0/0",
+						PeerGroup: "route-collector",
+					},
+				}); err != nil {
+					log.Printf("add dyn neighbor: %v", err)
+					if err := bgp.CleanUp(); err != nil {
+						log.Printf("cleaup: %v", err)
+					}
+					return
+				}
+			} else {
+				peerASN, err := utils.MustASN(m["peer-asn"])
+				if err != nil {
+					log.Printf("peer-asn is not valid asn: %v", err)
+					if err := bgp.CleanUp(); err != nil {
+						log.Printf("cleaup: %v", err)
+					}
+					return
+				}
+				neighborAddr, ok := m["neighbor-addr"].(string)
+				if !ok {
+					log.Printf("neighbor-addr is not string")
+					if err := bgp.CleanUp(); err != nil {
+						log.Printf("cleaup: %v", err)
+					}
+					return
+				}
+				if _, err := netip.ParseAddr(neighborAddr); err != nil {
+					log.Printf("invalid neighbor-addr: %s", neighborAddr)
+					if err := bgp.CleanUp(); err != nil {
+						log.Printf("cleaup: %v", err)
+					}
+					return
+				}
+				if err := bgpServer.AddPeer(context.Background(), &apipb.AddPeerRequest{
+					Peer: &apipb.Peer{
+						Conf: &apipb.PeerConf{
+							PeerAsn:         uint32(peerASN),
+							NeighborAddress: neighborAddr,
+						},
+						EbgpMultihop: &apipb.EbgpMultihop{
+							Enabled: true,
+						},
+						AfiSafis: []*apipb.AfiSafi{{
+							Config: &apipb.AfiSafiConfig{
+								Family: &apipb.Family{
+									Afi:  apipb.Family_AFI_IP,
+									Safi: apipb.Family_SAFI_UNICAST,
+								},
+							},
+							AddPaths: &apipb.AddPaths{
+								Config: &apipb.AddPathsConfig{
+									Receive: true,
+								},
+							},
+						}},
+					},
+				}); err != nil {
+					log.Printf("add peer fail: %s", neighborAddr)
+					if err := bgp.CleanUp(); err != nil {
+						log.Printf("cleaup: %v", err)
+					}
+				}
 			}
-		}
+		}()
 
-		return &BGP{
-			s: bgpServer,
-		}, nil
+		return bgp, nil
 	})
 }
 
 type BGP struct {
 	fetch.Base
-	s *server.BgpServer
+	s      *server.BgpServer
+	cancel context.CancelFunc
 }
 
 func (f *BGP) GetData() (any, error) {
@@ -201,7 +231,13 @@ func (f *BGP) GetData() (any, error) {
 }
 
 func (f *BGP) CleanUp() error {
-	f.s.StopBgp(context.Background(), nil)
+	if f.cancel != nil {
+		f.cancel()
+	}
+	err := f.s.StopBgp(context.Background(), &apipb.StopBgpRequest{})
+	if err != nil {
+		log.Printf("stop bgp: %v", err)
+	}
 	f.s.Stop()
 	return nil
 }
--- backend/graph/fetch/bgp/bgp_test.go
+++ backend/graph/fetch/bgp/bgp_test.go
@@ -11,10 +11,10 @@ import (
 func TestBGPDial(t *testing.T) {
 	bgp, err := fetch.Spawn["bgp"](map[string]any{
 		"mode":          "dial",
-		"asn":           4211110102,
-		"router-id":     "172.16.4.100",
+		"asn":           2,
+		"router-id":     "172.16.4.102",
 		"peer-asn":      4211110101,
-		"neighbor-addr": "172.16.4.6",
+		"neighbor-addr": "172.16.4.4",
 	})
 	if err != nil {
 		t.Error(err)
@@ -31,7 +31,7 @@ func TestBGPDial(t *testing.T) {
 func TestBGPListen(t *testing.T) {
 	bgp, err := fetch.Spawn["bgp"](map[string]any{
 		"mode":      "listen",
-		"asn":       4211110102,
+		"asn":       2,
 		"router-id": "172.16.4.100",
 	})
 	if err != nil {
