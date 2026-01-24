10e5b621f651d26825f57be08cbcacc5c369ae2b
BaiMeow <baishu2003@outlook.com>
2026-01-25 01:16:23 +0800

    Adjust width algo

--- frontend/src/utils/edge_width.ts
+++ frontend/src/utils/edge_width.ts
@@ -2,5 +2,5 @@ export function calcEdgeWidthFromPathBetweenness(
   betweenness: number,
   size: number = 1,
 ) {
-  return Math.max(betweenness * size * 2, 1)
+  return Math.max(betweenness * size * 2, 0.5)
 }
